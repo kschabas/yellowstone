4bdabe19c1614ddc19360b0fa5fedb2840e97ec6
Karl Schabas <karl.schabas@utoronto.ca>
2024-05-23 16:22:25 -0400

    Added User Authentication

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -1,6 +1,15 @@
 class AssignmentsController < ApplicationController
 
+  before_action :authenticate_user!, only: [:show, :update]
+
   def index
     @assignments = Assignment.all
   end
+
+  def show
+  end
+
+  def update
+  end
+  
 end
