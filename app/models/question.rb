ce356bd28247ccdc9984270ab97c0a64b976fd02
Karl Schabas <karl.schabas@utoronto.ca>
2024-05-28 18:57:00 -0400

    Added color coding for right and wrong answers

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,12 @@
 class Question < ApplicationRecord
   belongs_to :assignment
   has_many :submissions
+
+  def match?(guess)
+    if guess.nil?
+      return false
+    end
+    guess.downcase.include?(self.answer.downcase)
+  end
+
 end
