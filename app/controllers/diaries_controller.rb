f32880bc3e657473d4e53c566f28dc3076426911
Karl Schabas <karl.schabas@utoronto.ca>
2024-05-31 15:49:17 -0400

    Added basic create and update functions

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -3,7 +3,42 @@ class DiariesController < ApplicationController
     redirect_to new_diary_path
   end
 
-  def new
-    @diary = Diary.new
+  def show
+    redirect_to edit_diary_path
   end
+
+  def edit
+    @diary = Diary.find(params[:id])
+    render :new
+  end
+
+  def new
+    @date = Date.today
+    entry = Diary.where(user_id: current_user.id, date: @date)
+    if  entry.count == 0
+      @diary = Diary.new
+      @diary.date = @date
+    else
+      @diary = entry.first
+    end
+    
+  end
+
+  def create
+    @diary = Diary.new(diary_params)
+
+    @diary.save
+    redirect_to @diary
+  end
+
+  def update
+    @diary = Diary.find(params[:id])
+    @diary.update(diary_params)
+    redirect_to @diary
+  end
+
+  private
+    def diary_params
+      params.require(:diary).permit(:date, :did, :learnt, :user_id)
+    end
 end
