bc30b7d622ea7c4afe4ae205d69fd81a5ad366f4
Karl Schabas <karl.schabas@utoronto.ca>
2024-06-01 19:08:52 -0400

    Require authentication for diaries and scorecards

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,4 +1,6 @@
 class DiariesController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @date = Date.today
     @diary = Diary.find_by_date(@date,current_user.id)
--- app/controllers/scorecards_controller.rb
+++ app/controllers/scorecards_controller.rb
@@ -1,4 +1,5 @@
 class ScorecardsController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
   def index
     @assignments = Assignment.all
   end
